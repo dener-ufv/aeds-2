08b9a5ff20bcc9b99beb0ba42a27e4f02fddd1c2
Fabio Trindade <fabiotrindadefer@gmail.com>
2020-10-19 18:53:19 -0300

    Implementando comparações e tempo de execução da PATRICIA

--- trabalho-pratico-1/src/main.c
+++ trabalho-pratico-1/src/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "includes/patricia.h"
 #include "includes/patricia_if.h"
+#include "includes/benckmark.h"
 
 int main() {
-    
+    Benchmark benchPatricia;
     Word w;
     wordInit(&w);
 
     Patricia tree;
     patriciaInit(&tree);
+    benchmarkInit(&benchPatricia);
 
     int N = 5;
     char* list[] = {"amor", "amarelo", "baleia", "amar", "bala"};
@@ -16,10 +18,17 @@ int main() {
     int i;
     for(i=0; i<N; i++) {
         wordSetString(&w, list[i]);
-        patriciaInsert(&tree, w, NULL);
+        patriciaInsert(&tree, w, &benchPatricia);
     }
 
+    benchmarkStartTimer(&benchPatricia);
     patriciaPrintWords(tree);
+    benchmarkStopTimer(&benchPatricia);
+
+
+    printf("Tempo de execução patricia:%lf \n",benchmarkGetTime(benchPatricia) );
+    printf("Comparações Patricia:%d \n",benchmarkGetComparations(benchPatricia));
+
 
     return 0;
 }
