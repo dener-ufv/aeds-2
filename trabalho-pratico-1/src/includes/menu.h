7a6efb3479417c664e7ce6999a434cdee6a903e2
Lucas Takeshi <42739082+LTaka@users.noreply.github.com>
2020-10-26 20:15:35 -0300

    pequisa nao funciona e comparacao de pesquisa faltando

--- trabalho-pratico-1/src/includes/menu.h
+++ trabalho-pratico-1/src/includes/menu.h
@@ -2,7 +2,7 @@
   Grupo Divididos
   3869 - Fábio Trindade Ramos
   3872 - Dener Vieira Ribeiro
-  2665 - Lukas Takeshi Moreira Chang
+  2665 - Lucas Takeshi Moreira Chang
   3889 - Igor de Melo Alves
 */
 
